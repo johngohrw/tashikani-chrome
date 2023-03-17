6d2668b66405c9e3c47ecf364041876b8b004fcd
John Goh <johngohrw@gmail.com>
2023-03-17 16:38:44 +0800

    feat: CaptionEngine as a class

--- scripts/youtube/main.js
+++ scripts/youtube/main.js
@@ -1,11 +1,43 @@
 import { debug, isWatch, pathChecker } from "../utils.js";
 
+
+class CaptionEngine {
+  constructor() {
+    this.url = null
+    this.active = false
+  }
+
+  activate(url) {
+    this.active = true
+    this.url = url
+
+    console.log(document)
+
+    debug("CaptionEngine", "activate!", url)
+  }
+
+  deactivate() {
+    if (this.active) {
+      this.url = null
+      this.active = false
+
+      debug("CaptionEngine", "deactivate!")
+    }
+  }
+}
+
+
+const caption = new CaptionEngine()
+
+
 const onPathChange = function (current, previous) {
   debug("onPathChange", current);
   if (isWatch(current)) {
     console.log("watch!");
+    caption.activate(current)
   } else {
     console.log("not watch...");
+    caption.deactivate()
   }
 };
 
