19d731dce36313a781e9b1920158ac3cf4d2b9c7
John Goh <johngohrw@gmail.com>
2023-03-20 03:59:05 +0800

    wip: failed segment observer

--- scripts/youtube/main.js
+++ scripts/youtube/main.js
@@ -6,31 +6,57 @@ import { debug, isWatch, pathChecker, waitForNode } from "../utils/index.js";
 
 const YOUTUBE_CAPTION_CONTAINER_ID = "ytp-caption-window-container";
 const YOUTUBE_CAPTION_SEGMENT_CLASS = "ytp-caption-segment";
+const YOUTUBE_CAPTION_TOP = "ytp-caption-window-top";
+const YOUTUBE_CAPTION_BOTTOM = "ytp-caption-window-bottom";
 
 function hijackCaptions(callback) {
   const observerConfig = {
     childList: true,
-    subtree: true,
+    // subtree: true,
   };
 
   const targetNode = document.getElementById(YOUTUBE_CAPTION_CONTAINER_ID);
   const observer = new MutationObserver((mutationList, observer) => {
-    for (const mutation of mutationList) {
-      const classList = Array.from(mutation.target.classList);
-      if (classList.includes(YOUTUBE_CAPTION_SEGMENT_CLASS)) {
-        const captionEl = mutation.target;
+    // console.log(">", mutationList);
+    mutationList.forEach((mutation) => {
+      // console.log("a>", mutation);
+      const addedNodes = Array.from(mutation.addedNodes);
+      for (const captionTypeNode of addedNodes) {
+        const classList = Array.from(captionTypeNode.classList);
+        if (classList.includes(YOUTUBE_CAPTION_BOTTOM)) {
+          console.log(captionTypeNode);
 
-        // Disconnect it temporarily while we make changes to the observed element.
-        // An infinite loop will occur otherwise.
-        observer.disconnect();
+          const segmentObserver = new MutationObserver(
+            (_mutationList, observer) => {
+              _mutationList.forEach((_mutation) => {
+                console.log("segment>", _mutation.target.innerText);
+                const classList = Array.from(_mutation.target.classList);
 
-        // Process mutations
-        callback(captionEl);
+                if (classList.includes(YOUTUBE_CAPTION_SEGMENT_CLASS)) {
+                  const captionEl = _mutation.target;
+                  // Disconnect it temporarily while we make changes to the observed element.
+                  // An infinite loop will occur otherwise.
+                  segmentObserver.disconnect();
+                  // Process mutations
+                  callback(captionEl);
+                  // Re-observe the element.
+                  segmentObserver.observe(captionTypeNode, {
+                    childList: true,
+                    subtree: true,
+                  });
+                }
+              });
+            }
+          );
 
-        // Re-observe the element.
-        observer.observe(targetNode, observerConfig);
+          console.log(
+            captionTypeNode.getElementsByClassName(
+              YOUTUBE_CAPTION_SEGMENT_CLASS
+            )
+          );
+        }
       }
-    }
+    });
   });
 
   observer.observe(targetNode, observerConfig);
