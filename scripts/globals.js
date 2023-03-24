993dfe6bdc87f7aa3fce993fce5c0ad342914759
John Goh Rengwu <johngohrw@gmail.com>
2023-03-25 02:18:47 +0800

    feat: settings menu item injection

--- scripts/globals.js
+++ scripts/globals.js
@@ -1,5 +1,13 @@
 export const IS_DEBUG = true;
 export const YOUTUBE_CAPTION_CONTAINER_ID = "ytp-caption-window-container";
 export const YOUTUBE_CAPTION_SEGMENT_CLASS = "ytp-caption-segment";
+export const YOUTUBE_PLAYER_PANEL_MENU_CLASS = "ytp-panel-menu";
 export const YOUTUBE_CAPTION_TOP = "ytp-caption-window-top";
 export const YOUTUBE_CAPTION_BOTTOM = "ytp-caption-window-bottom";
+export const YOUTUBE_SETTINGS_MENU_CONTAINER = "ytp-settings-menu";
+export const YOUTUBE_SETTINGS_MENU_BUTTON = "ytp-settings-button";
+
+export const TASHIKANI_MENU_ITEM_ID = "tashikani-injected-menu-item"
+
+
+export const TASHIKANI_MENU_ITEM_LABEL = "Tashikani Settings"
--- scripts/utils/captions.js
+++ scripts/utils/captions.js
@@ -1,9 +1,14 @@
 import {
+  TASHIKANI_MENU_ITEM_ID,
+  TASHIKANI_MENU_ITEM_LABEL,
   YOUTUBE_CAPTION_CONTAINER_ID,
   YOUTUBE_CAPTION_SEGMENT_CLASS,
   YOUTUBE_CAPTION_TOP,
+  YOUTUBE_PLAYER_PANEL_MENU_CLASS,
+  YOUTUBE_SETTINGS_MENU_BUTTON,
+  YOUTUBE_SETTINGS_MENU_CONTAINER,
 } from "../globals.js";
-import { debug, getByClass } from "./index.js";
+import { debug, getByClass, waitForNode } from "./index.js";
 
 export function hijackCaptions(callback) {
   const observerConfig = {
@@ -125,7 +130,7 @@ function createCaptions() {
   return [captionEl, styles]
 }
 
-export function highlightableCaptions({ }) {
+export function highlightableCaptions() {
   debug("highlightableCaptions", "start hijacking");
 
   function wordHover(event) {
@@ -146,6 +151,11 @@ export function highlightableCaptions({ }) {
   document.getElementById(YOUTUBE_CAPTION_CONTAINER_ID).appendChild(panel);
   document.body.appendChild(panelStyles);
 
+  // inject menu item (only if it doesnt exist yet)
+  if (!document.getElementById(TASHIKANI_MENU_ITEM_ID)) {
+    menuItemInjection()
+  }
+
   return hijackCaptions((captionEl) => {
     //TODO: very naive word splitting implementation here
     const captionWords = captionEl.innerText
@@ -160,3 +170,111 @@ export function highlightableCaptions({ }) {
     });
   });
 }
+
+// export function hijackCaptions(callback) {
+//   const observerConfig = {
+//     childList: true,
+//     subtree: true,
+
+//   };
+
+//   const targetNode = document.getElementById(YOUTUBE_CAPTION_CONTAINER_ID);
+//   const observer = new MutationObserver((mutationList, observer) => {
+//     mutationList.forEach((mutation) => {
+//       const classList = Array.from(mutation.target.classList);
+//       if (
+//         classList.includes(YOUTUBE_CAPTION_SEGMENT_CLASS) &&
+//         !Array.from(mutation.target.offsetParent?.classList || []).includes(
+//           YOUTUBE_CAPTION_TOP
+//         )
+//       ) {
+//         // Disconnect it temporarily while we make changes to the observed element.
+//         // An infinite loop will occur otherwise.
+//         observer.disconnect();
+//         // Process mutations
+//         callback(mutation.target);
+//         // Re-observe the element.
+//         observer.observe(targetNode, observerConfig);
+//       }
+//     });
+//   });
+
+//   observer.observe(targetNode, observerConfig);
+//   return observer;
+// }
+
+function handleShowSettings(e) {
+  console.log("show settings > ", e)
+}
+
+function menuItemInjection() {
+  nodeCheckInterval = waitForNode({
+    document,
+    className: YOUTUBE_PLAYER_PANEL_MENU_CLASS,
+    callback: (elements) => {
+      if (elements.length > 0) {
+        injectMenuItem(elements[0])
+      } else {
+        console.error("failed to find class", YOUTUBE_PLAYER_PANEL_MENU_CLASS)
+      }
+    },
+    checkInterval: 250
+  });
+}
+
+function injectMenuItem(targetNode) {
+  debug("injectMenuItem", "observer started, getting ready to inject");
+
+  const observerConfig = {
+    attributes: true
+  };
+  const observer = new MutationObserver((mutationList, observer) => {
+    let shouldSkip = false
+    mutationList.forEach(mutation => {
+      if (!shouldSkip && mutation.type === "attributes" && mutation.attributeName === "style") {
+        // done observing.
+        observer.disconnect()
+        shouldSkip = true
+
+        // clone from existing menu item
+        const newMenuItem = mutation.target.childNodes[0].cloneNode(true)
+        const newMenuHeight = mutation.target.clientHeight + mutation.target.childNodes[0].clientHeight
+
+        // remove unnecessary attributes
+        newMenuItem.removeAttribute("role")
+        newMenuItem.removeAttribute("aria-checked")
+        newMenuItem.removeAttribute("aria-haspopup")
+        newMenuItem.removeAttribute("tabindex")
+        newMenuItem.id = TASHIKANI_MENU_ITEM_ID
+
+        // alter cloned menu item node
+        newMenuItem.childNodes[0].innerHTML = tashikaniIconString
+        newMenuItem.childNodes[1].innerText = TASHIKANI_MENU_ITEM_LABEL
+        newMenuItem.childNodes[1].setAttribute("style", "white-space: nowrap")
+        newMenuItem.childNodes[2].innerHTML = ""
+        newMenuItem.onclick = (e) => {
+          handleShowSettings(e)
+          console.log(document.getElementsByClassName(YOUTUBE_SETTINGS_MENU_BUTTON))
+          document.getElementsByClassName(YOUTUBE_SETTINGS_MENU_BUTTON)[0].click()
+        }
+
+        // append new menu item
+        mutation.target.appendChild(newMenuItem)
+
+        // alter parent container height to prevent scrollbar
+        // mutation.target.style.height = `${newMenuHeight}px`
+        // mutation.target.parentNode.style.height = `${newMenuHeight}px`
+        // mutation.target.parentNode.parentNode.style.height = `${newMenuHeight}px`
+
+      }
+    })
+  })
+  observer.observe(targetNode, observerConfig);
+}
+
+const tashikaniIconString = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <text fill="white" xml:space="preserve" style="white-space: pre" font-family="Inter" font-size="20"
+        letter-spacing="0em">
+        <tspan x="2" y="19.2727">&#x78ba; </tspan>
+    </text>
+  </svg>`
